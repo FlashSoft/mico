8a6a816aaf48b0de963d5d32b3dee05ef3cc2e13
FlashSoft <babyworld@gmail.com>
2018-10-23 15:19:03 +0800

    米爱不支持厌恶

--- install.sh
+++ install.sh
@@ -8,32 +8,32 @@ mico_rcpath="${root}/etc/rc.d/S96mico_enable"
 rm $mico_initpath
 rm $mico_rcpath
 echo ""
-echo "欢迎使用'\033[37m小爱拦截器\033[0m'安装工具 v0.1"
+echo "欢迎使用'小爱拦截器'安装工具 v0.1"
 echo ""
 echo "本工具通过拦截小爱的识别词和响应词"
 echo "把拦截的请求转发给NodeRed服务进行自定义设备的操作"
 echo ""
 
 # 环境检测,必须为小爱环境才继续
-[ -z "`uname -a|grep mico`" ] && echo "\033[33m当前不是小爱设备,请到小爱上执行此命令\033[0m" && exit
+[ -z "`uname -a|grep mico`" ] && echo "当前不是小爱设备,请到小爱上执行此命令" && exit
 
-echo "请输入响应拦截词,多个拦截词使用|分割,默认值为[\033[37m未知|没有\033[0m]:"
+echo "请输入响应拦截词,多个拦截词使用|分割,默认值为[未知|没有]:"
 read -p "" keywords
 [ -z "${keywords}" ] && keywords="未知|没有"
 
-echo "请输入响应拦截词的更新频率,单位秒,0为不更新,默认值[\033[37m0\033[0m]:"
+echo "请输入响应拦截词的更新频率,单位秒,0为不更新,默认值[0]:"
 read -p "" keywords_update_timeout
 [ -z "${keywords_update_timeout}" ] && keywords_update_timeout=0
 
-echo "请输入NodeRed服务地址,默认值[\033[37mhttp://192.168.1.1:1880/miai\033[0m]:"
+echo "请输入NodeRed服务地址,默认值[http://192.168.1.1:1880/miai]:"
 read -p "" nodered_url
 [ -z "${nodered_url}" ] && nodered_url="http://192.168.1.1:1880/miai"
 
 echo "==============================================================="
 echo ""
-echo "           响应拦截词: \033[33m${keywords}\033[0m"
-echo "   响应拦截词更新频率: \033[33m${keywords_update_timeout}\033[0m"
-echo "      NodeRed服务地址: \033[33m${nodered_url}\033[0m"
+echo "           响应拦截词: ${keywords}"
+echo "   响应拦截词更新频率: ${keywords_update_timeout}"
+echo "      NodeRed服务地址: ${nodered_url}"
 echo ""
 echo "==============================================================="
 
@@ -43,7 +43,7 @@ read -p "" enterkey
 # 下载远程脚本并检查是否成功
 mico=`curl -s 'https://raw.githubusercontent.com/FlashSoft/mico/master/mico.sh'`
 if [[ -z `echo "${mico}"|awk 'match($0,/VERSION/){print 1}'` ]];then
-  echo "\033[33m脚本下载不成功,可能你需要买个番茄先\033[0m"
+  echo "脚本下载不成功,可能你需要买个番茄先"
   exit
 fi
 
