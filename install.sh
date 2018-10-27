47c06421b562e91b757e452a96336369f4f942be
FlashSoft <fangchao@staff.weibo.com>
2018-10-27 19:58:01 +0800

    调整捕捉逻辑

--- install.sh
+++ install.sh
@@ -113,8 +113,8 @@ stop() {
   kill \`ps|grep 'sh ${mico_path}'|grep -v grep|awk '{print \$1}'\`
 }" > $mico_initpath
 chmod a+x $mico_initpath
-$mico_initpath enable
-$mico_initpath stop
+$mico_initpath enable > /dev/null 2>&1
+$mico_initpath stop > /dev/null 2>&1
 
 echo "安装完毕"
 echo "可以使用/etc/init.d/mico_enable start 启动小爱拦截器"
